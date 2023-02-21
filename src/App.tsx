6a9fb492bba27d9e1242aa0cd75fd254efd8b8eb
loyal_goose <polina.alina.2015@mail.ru>
2023-02-21 20:13:20 +0700

    feat: добавить страницу списка пицц

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { HomePage } from '@pages/HomePage/HomePage';
 
 import './App.scss';
 import { Layout } from '@components/Layout/Layout';
+import { PizzasPage } from '@pages/PizzasPage/PizzasPage';
 
 export const App = () => {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
+        <Route path='/pizzas' element={<PizzasPage />}></Route>
       </Routes>
     </Layout>
   );
---
+++ src/pages/PizzasPage/PizzasPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PizzasPage.scss';
+
+export const PizzasPage = () => {
+  return (
+    <div className='pizzas-wrapper'>
+      <p>Выбрать пиццу</p>
+      category
+      <div className='pizzas-items'></div>
+    </div>
+  );
+};
