c4849d0a6f149560e4549d56f6036585b8521cba
loyal_goose <polina.alina.2015@mail.ru>
2023-02-21 18:46:32 +0700

    feat: добавить изображения ингредиентов

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const HomePage = () => {
+  const speciesArray = [
+    '/files/homepage/olives.png',
+    '/files/homepage/cheese.png',
+    '/files/homepage/pepper.png',
+    '/files/homepage/pepperoni.png',
+    '/files/homepage/bacon.png',
+    '/files/homepage/chicken.png',
+    '/files/homepage/beef.png',
+    '/files/homepage/sausages.png'
+  ];
+
   return <div>Home</div>;
 };
