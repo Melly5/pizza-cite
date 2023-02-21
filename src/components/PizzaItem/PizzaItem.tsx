ce1ef85ddc4ebf2a9afadeffefc422b64cada2fb
loyal_goose <polina.alina.2015@mail.ru>
2023-02-21 23:19:38 +0700

    feat: добавить компонент пиццы

---
+++ src/components/PizzaItem/PizzaItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './PizzaItem.scss';
+
+export const PizzaItem = ({ pizza }: any) => {
+  return (
+    <div className='pizza-item-wrapper'>
+      <img className='image' src={pizza.img} alt='pizza' />
+      <div className='name'>
+        <h4 className='title'>{pizza.name}</h4>
+        <div>{pizza.price.size.medium} см</div>
+      </div>
+      <div className='ingredients'>
+        {pizza.ingredients.map((ingredient: string, id: number) => (
+          <>{ingredient}, </>
+        ))}
+      </div>
+      <div className='bottom'>
+        <div className='price'>{pizza.price.default} ₽</div>
+        <button className='button active'>Добавить</button>
+      </div>
+    </div>
+  );
+};
