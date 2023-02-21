161068c8d81b694ff504e1d3c4f649cc93fb24be
loyal_goose <polina.alina.2015@mail.ru>
2023-02-21 20:16:29 +0700

    feat: добавить компонент категории пицц

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Categories.scss';
+
+export const Categories = ({ value, onClickCategory }: any) => {
+  const categories = ['Рекомендуем', 'Новинки', 'Без мяса', 'Острая', 'Акции'];
+
+  return (
+    <div className='categories'>
+      <ul>
+        {categories.map((category, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={value === i ? 'button active' : 'button'}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/pages/PizzasPage/PizzasPage.tsx
+++ src/pages/PizzasPage/PizzasPage.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Categories } from '@components/Categories/Categories';
 import './PizzasPage.scss';
 
 export const PizzasPage = () => {
+  const [categoryId, setCategoryId] = React.useState<any>(0);
+  const categoryNames = ['new', 'spicy', 'vegeterian'];
+  console.log(categoryNames[categoryId]);
+
   return (
     <div className='pizzas-wrapper'>
       <p>Выбрать пиццу</p>
-      category
+      <Categories
+        value={categoryId}
+        onClickCategory={(id: number) => {
+          setCategoryId(id);
+        }}
+      />
       <div className='pizzas-items'></div>
     </div>
   );
